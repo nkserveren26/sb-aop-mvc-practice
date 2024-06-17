c09492166dd8163587277f8612f130f2c2611c65
nkserveren26 <norinoritoshi@outlook.jp>
2024-06-18 08:44:52 +0900

    Controller、Service、DAOを実行後に起動するAdviceを定義

--- src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -49,5 +50,19 @@ public class DemoLoggingAspect {
         for (Object tempArg : args) {
             myLogger.info("====>> argument: " + tempArg);
         }
+    }
+
+    // add @AfterReturning advice
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("====>> in @AfterReturning: from method: " + theMethod);
+
+        // display data returned
+        myLogger.info("====>> result: " + theResult);
     }
 }
